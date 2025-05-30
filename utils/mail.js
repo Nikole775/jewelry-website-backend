bbaf39a9bd40614a0a2372cfc12c61b94d1a6edc
Nikole_Labo914 <147343602+Nikole775@users.noreply.github.com>
2025-05-31 02:16:37 +0300

    Update mail.js

--- utils/mail.js
+++ utils/mail.js
@@ -12,8 +12,8 @@ async function sendVerificationEmail(to, code) {
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to,
-        subject: 'Your Login Verification Code',
-        text: Your verification code is: ${code},
+        subject:'Your Login Verification Code',
+        text:'Your verification code is: ${code}',
     };
 
     await transporter.sendMail(mailOptions);
