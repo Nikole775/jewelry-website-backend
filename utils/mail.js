12fe040248aee4b934eaee4355fd3871d12cc5c1
Nikole_Labo914 <147343602+Nikole775@users.noreply.github.com>
2025-05-31 00:39:35 +0300

    Create mail.js

---
+++ utils/mail.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail', // or another SMTP service
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+async function sendVerificationEmail(to, code) {
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to,
+        subject: 'Your Login Verification Code',
+        text: Your verification code is: ${code},
+    };
+
+    await transporter.sendMail(mailOptions);
+}
+
+module.exports = { sendVerificationEmail };
