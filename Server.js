7dd66bf704b265981bdd85b0f7c8084bb32af791
Nikole_Labo914 <147343602+Nikole775@users.noreply.github.com>
2025-05-31 18:52:04 +0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -287,7 +287,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://jewelry-website-frontend.onrender.com", // Your frontend URL
+    credentials: true,
+}));
 app.use(express.json());
 app.use('/uploads', express.static(uploadPath));
 
