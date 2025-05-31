503384b1c654bfb9055e1efbd9abe470ffbbfb0b
Nikole_Labo914 <147343602+Nikole775@users.noreply.github.com>
2025-05-31 23:45:16 +0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 // routes/authRoutes.js
 import express from 'express';
-import { login, register } from '../controller/authController.js'; // Adjust path if needed
+import { login, register, verifyCode } from '../controller/authController.js'; // Adjust path if needed
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/register', register);
+router.post('/verify-code', verifyCode);
 
 export default router;
